804fcbe01645f9920d68e3f24f205cce9d30c85b
Matthew Leong <mleong94@gmail.com>
2014-11-02 19:36:30 -0800

    Part 2

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,2 +1,8 @@
 class PokemonController < ApplicationController
+	def capture
+		currPkmn = Pokemon.find(params[:id])
+		currPkmn.trainer_id = current_trainer.id
+		currPkmn.save
+		redirect_to root_path
+	end
 end
